c3ee4fbfea6318d2d1f93092733cf7a087091e8f
Tiago Sansão <tiagossansao@gmail.com>
2021-06-23 19:52:36 -0300

    Finished algorithm that creates rows and buttons in the game

--- pages/game.jsx
+++ pages/game.jsx
@@ -1,23 +1,40 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar, Platform, Pressable } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, Platform, Pressable, Dimensions } from 'react-native';
+
+const {height, width} = Dimensions.get("window");
 
 export default function game() {
 
 
   function buttons() {
     let buttonsQuantity = 9;
+    let RowQuantity = 3;
     let buttonsArr = [];
-    for (let i = 0; i < buttonsQuantity; i += 1) {
-      buttonsArr.push(<Pressable key={`btn${i}`} onPress={() => {}} style={({ pressed }) => [{backgroundColor: pressed ? 'black' : 'blue'}, styles.button]} />)
+    let buttonsWithRowsArr = [];
+
+    for (let i = 0; i < RowQuantity; i += 1) {
+      buttonsWithRowsArr.push([]);
     }
-    return buttonsArr
+
+    for (let i = 0; i < buttonsQuantity; i += 1) {
+      buttonsArr.push(<Pressable key={`btn${i}`} onPress={() => {}} style={({ pressed }) => [{backgroundColor: pressed ? '#e76f51' : '#e9c46a'}, styles.button]} />)
+    }
     
+    buttonsWithRowsArr.forEach((row, index) => {
+      for (let i = 0; i < RowQuantity; i += 1) {
+        row.push(buttonsArr.shift())
+      }
+    });
+    
+    return buttonsWithRowsArr;
   }
 
   return (
   <SafeAreaView style={styles.container}>
     <Text>Pijas</Text>
-    <View style={styles.btnContainer}>{buttons()}</View>
+    <View style={styles.btnContainer}>{buttons().map(((rowArr, index) => {
+      return <View key={index} style={styles.row}>{rowArr}</View>
+    }))}</View>
   </SafeAreaView>
   )
 };
@@ -27,18 +44,29 @@ const styles = StyleSheet.create({
     marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     backgroundColor: '#fff',
     flexDirection: 'column',
+    backgroundColor: '#2a9d8f',
+    width: width,
+    height: height,
   },
   button: {
-    width: 100,
-    height: 100,
+    flex: 1,
+    margin: 5,
   },
   btnContainer: {
     display: 'flex',
-    width: '100%',
-    height: '100%',
+    width: width * 0.80,
+    height: width *0.80,
     justifyContent: 'space-around',
-    flexDirection: 'row',
+    flexDirection: 'column',
     flexWrap: 'wrap',
-
-  }
+    alignSelf: 'center',
+    backgroundColor: '#264653',
+    padding: 5,
+  },
+  row: {
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
 })
