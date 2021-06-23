0cd53127c8c7daae74e40655dde4627474b39eca
Tiago Sansão <tiagossansao@gmail.com>
2021-06-22 21:04:07 -0300

    Started stylizing game.jsx

--- pages/game.jsx
+++ pages/game.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar, Platform, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, Platform, Pressable } from 'react-native';
 
 export default function game() {
+
+
+  function buttons() {
+    let buttonsQuantity = 9;
+    let buttonsArr = [];
+    for (let i = 0; i < buttonsQuantity; i += 1) {
+      buttonsArr.push(<Pressable key={`btn${i}`} onPress={() => {}} style={({ pressed }) => [{backgroundColor: pressed ? 'black' : 'blue'}, styles.button]} />)
+    }
+    return buttonsArr
+    
+  }
+
   return (
   <SafeAreaView style={styles.container}>
     <Text>Pijas</Text>
+    <View style={styles.btnContainer}>{buttons()}</View>
   </SafeAreaView>
   )
 };
@@ -12,9 +25,20 @@ export default function game() {
 const styles = StyleSheet.create({
   container: {
     marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
   },
+  button: {
+    width: 100,
+    height: 100,
+  },
+  btnContainer: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+
+  }
 })
